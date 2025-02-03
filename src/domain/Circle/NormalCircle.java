972dbb7d05c98f173a78053ed7ad7cf6e6f33378
Parkyuchan <dbcks4848@naver.com>
2025-02-03 17:04:01 +0900

    정리

--- src/domain/Circle/NormalCircle.java
+++ src/domain/Circle/NormalCircle.java
@@ -4,8 +4,6 @@ import domain.Vehicle;
 
 public class NormalCircle extends Vehicle {
 
-    private boolean door;
-
     public NormalCircle() { super(0, 0, 2000); }
 
     public NormalCircle(int priceFuel, int useFuel, int money) {
